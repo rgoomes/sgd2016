43d811a8cebcd7a4290365435c8a680ed93a1449
rgoomes <ricardo.jpg@hotmail.com>
2016-02-20 23:22:11 +0000

    memory free parser

--- BigData/parse.py
+++ BigData/parse.py
@@ -1,9 +1,6 @@
 import sys
 
 file_name = sys.argv[1]
-
 with open(file_name) as f:
-	content = f.readlines()
-
-for i in range(len(content)):
-	print content[i][:-2]
+    for line in f:
+        print line[:-2]
