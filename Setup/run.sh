4930c48f5cc27a1c9ed245b776b015b39ee520e4
rgoomes <ricardo.jpg@hotmail.com>
2016-02-20 17:40:20 +0000

    Fixed tabs

--- Setup/run.sh
+++ Setup/run.sh
@@ -8,9 +8,9 @@
 
 for i in `seq 1 ${1}`
 do
-  echo 'Run' $i
-  mysql -u root < drop.sql
-  time --format='gen took %e seconds' mysql -u root < gen.sql
-  time --format='load took %e seconds' mysql -u root < load.sql
-  echo
+    echo 'Run' $i
+    mysql -u root < drop.sql
+    time --format='gen took %e seconds' mysql -u root < gen.sql
+    time --format='load took %e seconds' mysql -u root < load.sql
+    echo
 done;
