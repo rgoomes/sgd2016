6ba286c25a0a99ee96f7a3980c924e363897493a
rgoomes <ricardo.jpg@hotmail.com>
2016-02-24 16:17:34 +0000

    Added queries

--- Setup/run_mysql.sh
+++ Setup/run_mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# USAGE: ./run_mysql.sh [number of runs]
-# EXAMPLE: ./run_mysql.sh 10
+# USAGE: ./run_postgre.sh [number of runs] [number of clients/queries]
+# EXAMPLE: ./run_postgre.sh 10 100
 
 # First disable mysql root password with:
 # SET PASSWORD FOR root@localhost=PASSWORD('');
@@ -15,8 +15,30 @@ do
     time --format='keys creation took %e seconds' mysql -u root < scripts/mysql/keys_mysql.sql
     echo
 
+    # warm up
+    time --format='test (1 querie to n clients) took: %e seconds' ./code/bench $2 teste.sql mysql 1 > /dev/null
+    time --format='test (n queries to 1 client) took: %e seconds' ./code/bench 1 teste.sql mysql $2 > /dev/null
+
     # benchmark
-    time --format='querie 1 (1 querie to n clients) took: %e seconds' ./code/bench 100 teste.sql mysql 1 > /dev/null
-    time --format='querie 1 (n queries to 1 client) took: %e seconds' ./code/bench 1 teste.sql mysql 100 > /dev/null
+    time --format='querie 1 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
+    time --format='querie 1 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
+    time --format='querie 2 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
+    time --format='querie 2 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
+    time --format='querie 3 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
+    time --format='querie 3 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
+    time --format='querie 4 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
+    time --format='querie 4 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
+    time --format='querie 5 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
+    time --format='querie 5 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
+    time --format='querie 6 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
+    time --format='querie 6 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
+    time --format='querie 7 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
+    time --format='querie 7 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
+    time --format='querie 8 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
+    time --format='querie 8 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
+    time --format='querie 9 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
+    time --format='querie 9 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
+    time --format='querie 10 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
+    time --format='querie 10 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
     echo
 done;
--- Setup/run_postgre.sh
+++ Setup/run_postgre.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# USAGE: ./run_postgre.sh [number of runs]
-# EXAMPLE: ./run_postgre.sh 10
+# USAGE: ./run_postgre.sh [number of runs] [number of clients/queries]
+# EXAMPLE: ./run_postgre.sh 10 100
 
 # forwarding postgre output to /dev/null
 # to clear output
@@ -15,8 +15,30 @@ do
     time --format='keys creation took %e seconds' psql < scripts/postgre/keys_postgre.sql > /dev/null
     echo
 
+    # warm up
+    time --format='test (1 querie to n clients) took: %e seconds' ./code/bench $2 teste.sql postgre 1 > /dev/null
+    time --format='test (n queries to 1 client) took: %e seconds' ./code/bench 1 teste.sql postgre $2 > /dev/null
+
     # benchmark
-    time --format='querie 1 (1 querie to n clients) took: %e seconds' ./code/bench 100 teste.sql postgre 1 > /dev/null
-    time --format='querie 1 (n queries to 1 client) took: %e seconds' ./code/bench 1 teste.sql postgre 100 > /dev/null
+    time --format='querie 1 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
+    time --format='querie 1 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
+    time --format='querie 2 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
+    time --format='querie 2 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
+    time --format='querie 3 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
+    time --format='querie 3 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
+    time --format='querie 4 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
+    time --format='querie 4 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
+    time --format='querie 5 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
+    time --format='querie 5 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
+    time --format='querie 6 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
+    time --format='querie 6 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
+    time --format='querie 7 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
+    time --format='querie 7 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
+    time --format='querie 8 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
+    time --format='querie 8 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
+    time --format='querie 9 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
+    time --format='querie 9 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
+    time --format='querie 10 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
+    time --format='querie 10 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
     echo
 done;
