c54e02bb393028c0924e7cef03db9bdbba6ba92b
rgoomes <ricardo.jpg@hotmail.com>
2016-03-01 14:02:04 +0000

    simplify scripts

--- Setup/run_mysql.sh
+++ Setup/run_mysql.sh
@@ -10,35 +10,24 @@ for i in `seq 1 ${1}`
 do
     echo '\033[0;32mRun' $i '\033[0m'
     mysql -u root < scripts/drop.sql
-    time --format='tables creation took %e seconds' mysql -u root < scripts/mysql/tables_mysql.sql
-    time --format='load data took %e seconds' mysql -u root < scripts/mysql/load_mysql.sql
-    time --format='keys creation took %e seconds' mysql -u root < scripts/mysql/keys_mysql.sql
+    time mysql -u root < scripts/mysql/tables_mysql.sql
+    time mysql -u root < scripts/mysql/load_mysql.sql
+    time mysql -u root < scripts/mysql/keys_mysql.sql
     echo
 
     # warm up
-    time --format='test (1 querie to n clients) took: %e seconds' ./code/bench $2 teste.sql mysql 1 > /dev/null
-    time --format='test (n queries to 1 client) took: %e seconds' ./code/bench 1 teste.sql mysql $2 > /dev/null
+    time ./code/bench 1 teste.sql mysql $2 > /dev/null
 
     # benchmark
-    time --format='querie 1 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
-    time --format='querie 1 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
-    time --format='querie 2 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
-    time --format='querie 2 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
-    time --format='querie 3 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
-    time --format='querie 3 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
-    time --format='querie 4 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
-    time --format='querie 4 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
-    time --format='querie 5 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
-    time --format='querie 5 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
-    time --format='querie 6 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
-    time --format='querie 6 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
-    time --format='querie 7 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
-    time --format='querie 7 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
-    time --format='querie 8 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
-    time --format='querie 8 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
-    time --format='querie 9 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
-    time --format='querie 9 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
-    time --format='querie 10 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql mysql 1 > /dev/null
-    time --format='querie 10 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql mysql $2 > /dev/null
+    time ./code/bench 1  1.sql mysql $2 > /dev/null
+    time ./code/bench 1  2.sql mysql $2 > /dev/null
+    time ./code/bench 1  3.sql mysql $2 > /dev/null
+    time ./code/bench 1  4.sql mysql $2 > /dev/null
+    time ./code/bench 1  5.sql mysql $2 > /dev/null
+    time ./code/bench 1  6.sql mysql $2 > /dev/null
+    time ./code/bench 1  7.sql mysql $2 > /dev/null
+    time ./code/bench 1  8.sql mysql $2 > /dev/null
+    time ./code/bench 1  9.sql mysql $2 > /dev/null
+    time ./code/bench 1 10.sql mysql $2 > /dev/null
     echo
 done;
--- Setup/run_postgre.sh
+++ Setup/run_postgre.sh
@@ -10,35 +10,24 @@ for i in `seq 1 ${1}`
 do
     echo '\033[0;32mRun' $i '\033[0m'
     psql < scripts/drop.sql > /dev/null
-    time --format='tables creation took %e seconds' psql < scripts/postgre/tables_postgre.sql > /dev/null
-    time --format='load data took %e seconds' psql < scripts/postgre/load_postgre.sql > /dev/null
-    time --format='keys creation took %e seconds' psql < scripts/postgre/keys_postgre.sql > /dev/null
+    time psql < scripts/postgre/tables_postgre.sql > /dev/null
+    time psql < scripts/postgre/load_postgre.sql > /dev/null
+    time psql < scripts/postgre/keys_postgre.sql > /dev/null
     echo
 
     # warm up
-    time --format='test (1 querie to n clients) took: %e seconds' ./code/bench $2 teste.sql postgre 1 > /dev/null
-    time --format='test (n queries to 1 client) took: %e seconds' ./code/bench 1 teste.sql postgre $2 > /dev/null
+    time ./code/bench 1 teste.sql postgre $2 > /dev/null
 
     # benchmark
-    time --format='querie 1 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
-    time --format='querie 1 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
-    time --format='querie 2 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
-    time --format='querie 2 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
-    time --format='querie 3 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
-    time --format='querie 3 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
-    time --format='querie 4 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
-    time --format='querie 4 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
-    time --format='querie 5 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
-    time --format='querie 5 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
-    time --format='querie 6 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
-    time --format='querie 6 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
-    time --format='querie 7 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
-    time --format='querie 7 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
-    time --format='querie 8 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
-    time --format='querie 8 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
-    time --format='querie 9 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
-    time --format='querie 9 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
-    time --format='querie 10 (1 querie to n clients) took: %e seconds' ./code/bench $2 1.sql postgre 1 > /dev/null
-    time --format='querie 10 (n queries to 1 client) took: %e seconds' ./code/bench 1 1.sql postgre $2 > /dev/null
+    time ./code/bench 1  1.sql postgre $2 > /dev/null
+    time ./code/bench 1  2.sql postgre $2 > /dev/null
+    time ./code/bench 1  3.sql postgre $2 > /dev/null
+    time ./code/bench 1  4.sql postgre $2 > /dev/null
+    time ./code/bench 1  5.sql postgre $2 > /dev/null
+    time ./code/bench 1  6.sql postgre $2 > /dev/null
+    time ./code/bench 1  7.sql postgre $2 > /dev/null
+    time ./code/bench 1  8.sql postgre $2 > /dev/null
+    time ./code/bench 1  9.sql postgre $2 > /dev/null
+    time ./code/bench 1 10.sql postgre $2 > /dev/null
     echo
 done;
